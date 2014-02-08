8679e4f2cf19fe9d4b39a3831af160703bdba1b1
Tom Hayward <esarfl+github@gmail.com>
2014-02-07 18:17:23 -0800

    Parse RFC 2253 format, used for SSL_CLIENT_S_DN in Apache >2.3.11.

--- django_ssl_auth/lotw.py
+++ django_ssl_auth/lotw.py
@@ -10,8 +10,21 @@
 Utility functions for handling certificates from ARRL Logbook of the World
 """
 
+def _decode_asn1_hack(asn1):
+    # sorry
+    return ''.join([chr(int(x, 16)) for x in map(''.join, zip(*[iter(asn1[5:])]*2))])
+
+
 def _dictify_dn(dn):
-    return dict(x.split('=') for x in dn.split('/') if '=' in x)
+    try:
+        return dict(x.split('=') for x in dn.split('/') if '=' in x)
+    except ValueError:
+        # Since version 2.3.11, Apache HTTPD uses a RFC 2253 compatible format
+        d = dict(x.split('=') for x in dn.split(',') if '=' in x)
+        if d['1.3.6.1.4.1.12348.1.1'].startswith('#13'):
+            d['1.3.6.1.4.1.12348.1.1'] = _decode_asn1_hack(d['1.3.6.1.4.1.12348.1.1'])
+        return d
+
 
 def user_dict_from_dn(dn):
     d = _dictify_dn(dn)
